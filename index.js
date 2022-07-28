593548d8017e09bc9f7312817226f2542cdcbb59
batuhanilgarr <batuhan.ilgar@8bitiz.com>
2022-07-28 18:06:26 +0300

    #

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-$('h1').css('color', 'red')
+// document.querySelector("h1");
+// document.querySelectorAll("button");
+// ile
+// $("h1")
+// $('button')
+// aynı şeyi çalıştırır
+
+// H1'in rengini değiştirme //
+// $("h1").css("color", "green");
+
+// h1'in rengini rgb olarak gösterme //
+// console.log($('h1').css('color'))
+
+// h1'in font boyutunu px olarak gösterme //
+// console.log($('h1').css('font-size'))
+
+// h1'in fontunu 5rem olarak değiştirme //
+// $('h1').css('font-size', '5rem')
+
+// Class Ekleme //
+// $('h1').addClass('big-title');
+// Class Silme //
+// $('h1').removeClass('big-title')
+
+//Class'ın işlendiğini anlamak için ; 
+// $('h1').hasClass('margin-50')
+
+
+// Birden fazla class tanımlamak için //
+// $('h1').addClass('big-title margin-50')
+
+// Elementin yazısını değiştirmek için // 
+// $("h1").text("Bye");
+
+// Text ismini HTML şeklinde değiştirmek için //
+// $('button').html('<em>Hey</em>')
+
+
+
+
+$('h1').text('Bye');
+
+$("button").html("<em>Hey</em>");
