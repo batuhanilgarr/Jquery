1f3ee7f5bc0a00f04cfdb95734fe2bd4a5d95288
batuhanilgarr <batuhan.ilgar@8bitiz.com>
2022-07-29 15:35:12 +0300

    JQuery

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// $('h1').text('Bye')
+
+// $('button').html('<em>Hey</em>')
+
 // document.querySelector("h1");
 // document.querySelectorAll("button");
 // ile
@@ -22,14 +26,13 @@
 // Class Silme //
 // $('h1').removeClass('big-title')
 
-//Class'ın işlendiğini anlamak için ; 
+//Class'ın işlendiğini anlamak için ;
 // $('h1').hasClass('margin-50')
-
 
 // Birden fazla class tanımlamak için //
 // $('h1').addClass('big-title margin-50')
 
-// Elementin yazısını değiştirmek için // 
+// Elementin yazısını değiştirmek için //
 // $("h1").text("Bye");
 
 // Text ismini HTML şeklinde değiştirmek için //
@@ -38,6 +41,34 @@
 
 
 
-$('h1').text('Bye');
+// 29.07.2022
 
-$("button").html("<em>Hey</em>");
+
+
+
+// Elemanın değerini almak ve değiştirmek için .attr //
+// console.log($("img").attr("src"));
+
+// a Elemanının değerini değiştirmek için //
+// $('a').attr('href', 'https://yahoo.com')
+
+// Eleman'ın üzerindeki class'ları sorgulamak için //
+// $('h1').attr('class')
+// 'big-title margin-50'
+
+
+// Function ile yazı rengi değiştirme //
+// $("h1").click(function(){
+//  $("h1").css("color", "purple");
+// })
+
+
+
+// Button sayısını tespit etme ve sonrasında tıklandığın h1 rengini değiştirme //
+for (var i = 0; i < 5; i++) {
+ document.querySelectorAll("button")[i].addEventListener("click", function(){
+  document.querySelector("h1").style.color = "purple"
+ });
+}
+
+// //
